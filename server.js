0dfe3642374929082737b655afac27a1b6e700ce
Adam Slater <adam.slater.1010@gmail.com>
2017-10-19 22:21:28 -0700

    keep track of users

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-let app  = require('express')();
-let http = require('http').Server(app);
-let io   = require('socket.io')(http);
+let app    = require('express')();
+let server = require('http').Server(app);
+let io     = require('socket.io')(server);
+
+let users = [];
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -9,11 +11,26 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
     console.log('a user connected');
 
+    // New User
+    socket.on('new user', function (user, callback) {
+        callback(true);
+        socket.username = user.username;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames() {
+        io.sockets.emit('get users', users);
+    }
+
+    // Disconnect
     socket.on('disconnect', function(){
+        users.splice(users.indexOf(socket.username, 1));
+        updateUsernames();
         console.log('user disconnected');
     });
 });
 
-http.listen(3000, function(){
+server.listen(3000, function(){
     console.log('listening on *:3000');
 });
