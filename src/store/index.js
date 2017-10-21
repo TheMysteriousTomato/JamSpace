fbde8f5dcb2efb46f270e5e8ebf077b191638cd4
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2017-10-21 01:09:14 -0700

    add super basic vuex store

---
+++ src/store/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    bandName: null,
+    connectedRoom: null,
+    user: null,
+  },
+  getters: {
+    isConnected: state => (state.bandName !== null && state.user !== null),
+  },
+  mutations: {
+    register: (state, payload) => {
+      state.bandName = payload.bandName;
+      state.connectedRoom = payload.bandName;
+      state.user = payload.user;
+    },
+  },
+  actions: {
+    registerUser({ commit }, payload) {
+      const user = payload.user;
+      const $socket = payload.$socket;
+
+      if (user && $socket) {
+        $socket.emit('new user', user, (success) => {
+          if (success) {
+            commit('register', { user: user.username, bandName: user.band, connectedRoom: null });
+          }
+        });
+      }
+    },
+  },
+});
