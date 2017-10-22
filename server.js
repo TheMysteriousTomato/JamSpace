179542b2b13bac4ced41663616f9746a7a6c7a84
Adam Slater <adam.slater.1010@gmail.com>
2017-10-22 02:22:26 -0700

    init layout for play page

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.get('/sounds/:instrument/:sound', function (req, res) {
     else res.sendFile(__dirname + `/sounds/${req.params.instrument}/${req.params.sound}`);
 });
 
+app.get("/play", function (req, res) {
+    res.sendFile(__dirname + "/templates/play.html");
+});
+
 io.on('connection', function(socket){
     connections.push(socket);
     console.log('Connected: %s sockets connected', connections.length);
