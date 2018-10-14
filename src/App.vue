03f2bb23c285c6a4af10b04f0183ccc28b0761ca
Adam Slater <adam.slater.1010@gmail.com>
2018-10-13 22:42:56 -0700

    externalized style sheets (partially)

--- src/App.vue
+++ src/App.vue
@@ -12,22 +12,15 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  position: fixed;
-  top: 0;
-  left: 0;
-  background: url('./assets/bg.jpg') no-repeat center;
-  background-size: cover;
-  width: 100%;
-  height: 100%;
+  @import './css/app.css';
 
-}
-dd, dt {
-  display: inline-block;
-}
+  #app {
+    background: url('./assets/bg.jpg') no-repeat center;
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+  }
+  dd, dt {
+    display: inline-block;
+  }
 </style>
