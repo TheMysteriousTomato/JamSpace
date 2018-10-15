14a096910a95be4b28653664c17536f7b70f31c0
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2018-10-14 22:18:55 -0700

    refactor vue loader plugin

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,7 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const { VueLoaderPlugin } = require('vue-loader')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
